33c1a18c02bf9a8a22b4b862ae6a50ba83464d07
vanjaanderson <vanja@websnabben.se>
2017-03-08 20:35:41 +0100

    Update from Websnabben

--- front-page.php
+++ front-page.php
@@ -17,30 +17,6 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		
-
-		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== olepod_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param $num_sections integer
-			 */
-			$num_sections = apply_filters( 'olepod_front_page_sections', 4 );
-			global $olepodcounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$olepodcounter = $i;
-				olepod_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== olepod_panel_count() ) ends here. ?>
-
 	</main><!-- #main -->
 </div><!-- #primary -->
 
